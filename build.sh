1a4d28a4524a430f24cc60dcbbc590cc76452cf8
iloveyou1500222222-glitch <iloveyou1500222222@gmail.com>
2026-07-29 10:31:29 +0630

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install FFmpeg using static build
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
+tar -xf ffmpeg.tar.xz
+mv ffmpeg-*-amd64-static/ffmpeg /usr/local/bin/ffmpeg
+mv ffmpeg-*-amd64-static/ffprobe /usr/local/bin/ffprobe
+chmod +x /usr/local/bin/ffmpeg
+chmod +x /usr/local/bin/ffprobe
+
+# Install Python dependencies
+pip install -r requirements.txt
