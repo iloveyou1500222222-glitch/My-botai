bb41292b0c48bbee31ad2902be9b80d7e3fa01d8
iloveyou1500222222-glitch <iloveyou1500222222@gmail.com>
2026-07-29 10:41:07 +0630

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install FFmpeg using static build
+# Install FFmpeg
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
 tar -xf ffmpeg.tar.xz
 mv ffmpeg-*-amd64-static/ffmpeg /usr/local/bin/ffmpeg
